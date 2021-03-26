2014d4e1d72bdd6881bbae5a9bc94e1c14e1950b
mohamedibrahim1990 <43402445+mohamedibrahim1990@users.noreply.github.com>
2021-03-26 15:35:41 +0200

    4.1 ownership start

---
+++ 4-UnderstandOwnership/4-1 what is ownership/ownership/target/rls/debug/deps/ownership-5582ca148f9131e1.d
@@ -0,0 +1,5 @@
+g:\Learn Rust\Rust-The-Book\4-UnderstandOwnership\4-1 what is ownership\ownership\target\rls\debug\deps\ownership-5582ca148f9131e1.rmeta: src\main.rs
+
+g:\Learn Rust\Rust-The-Book\4-UnderstandOwnership\4-1 what is ownership\ownership\target\rls\debug\deps\ownership-5582ca148f9131e1.d: src\main.rs
+
+src\main.rs:
---
+++ 4-UnderstandOwnership/4-1 what is ownership/ownership/target/rls/debug/deps/ownership-f471fe822fa06bc1.d
@@ -0,0 +1,5 @@
+g:\Learn Rust\Rust-The-Book\4-UnderstandOwnership\4-1 what is ownership\ownership\target\rls\debug\deps\ownership-f471fe822fa06bc1.rmeta: src\main.rs
+
+g:\Learn Rust\Rust-The-Book\4-UnderstandOwnership\4-1 what is ownership\ownership\target\rls\debug\deps\ownership-f471fe822fa06bc1.d: src\main.rs
+
+src\main.rs:
